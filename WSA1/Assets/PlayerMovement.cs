c79d52c5ea3e3b1a4d07a9d30273122de4e8c31f
hadhehog <hadhehog@gmail.com>
2020-07-25 01:20:08 +0200

    Animations
    Added:
    - Player Idle animation in different directions (Down, Left, Right, Up)
    - Player Walk animation in different directions
    Fixed:
    - Animator was added to the code of PlayerMovement controller

--- WSA1/Assets/PlayerMovement.cs
+++ WSA1/Assets/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D rb;
     private Vector3 change;
-    // Start is called before the first frame update
+    private Animator anim;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,10 +21,21 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
         change.y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+        MovementAnimationUpdate();
+    }
 
+    void MovementAnimationUpdate()
+    {
         if (change != Vector3.zero)
         {
             transform.Translate(new Vector3(change.x, change.y));
+            anim.SetFloat("moveX", change.x);
+            anim.SetFloat("moveY", change.y);
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
         }
     }
 }
