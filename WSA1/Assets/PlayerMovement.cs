6eeda4c2f7cdc88327c479884c9e3462aca4394d
hadhehog <hadhehog@gmail.com>
2020-07-25 00:42:10 +0200

    Player
    Added:
    - Player (Object)
    - A simple movement controller (for Player)
    Fixed:
    - Spritesheets quality was changed to High
    - Filter mode was changed to Point (no filter)

---
+++ WSA1/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector3 change;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        change.y = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+
+        if (change != Vector3.zero)
+        {
+            transform.Translate(new Vector3(change.x, change.y));
+        }
+    }
+}
