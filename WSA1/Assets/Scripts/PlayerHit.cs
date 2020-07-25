f04060db4f90c262be7af331a9fcaafbbb0b4556
hadhehog <hadhehog@gmail.com>
2020-07-25 16:58:58 +0200

    Destroyable Object
    Added:
    - New destroyable object (Pot)
    - Added animations for Pot
    - Added PlayerHit script for delivering damage
    - Added Pot script, that actually can be used for any simple destroyable object

---
+++ WSA1/Assets/Scripts/PlayerHit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHit : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Breakable"))
+        {
+            other.GetComponent<Pot>().Broken();
+        }
+    }
+}
---
+++ WSA1/Assets/Scripts/Pot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Broken()
+    {
+        anim.SetBool("isBroken", true);
+        StartCoroutine(breakCo());
+    }
+
+    IEnumerator breakCo()
+    {
+        yield return new WaitForSeconds(.3f);
+        this.gameObject.SetActive(false);
+    }
+}
