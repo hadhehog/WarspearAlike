07d9868950ca00dd6527dae8059bc190a3648070
hadhehog <hadhehog@gmail.com>
2020-07-27 03:42:31 +0200

    AI 1.01
    Fixed:
    - Enemy script (finished State Machine, added Damage and HP system, DeathEffect, "stun effect"
    - log script (added changing for states and changing animations, also finished CheckDistance function)
    Added:
    - attack animaton for Log
    - script LogAttack (still in progress have a trouble with a function override)

--- WSA1/Assets/Scripts/Enemy.cs
+++ WSA1/Assets/Scripts/Enemy.cs
@@ -13,11 +13,59 @@ public enum EnemyState
 public class Enemy : MonoBehaviour
 {
 
+    [Header("State Machine")]
     public EnemyState currentState;
-    public int health;
+
+    [Header("Enemy Stats")]
+    public FloatValue maxHealth;
+    public float health;
     public string enemyName;
     public int baseAttack;
     public float moveSpeed;
+    public Vector2 homePosition;
+
+    [Header("Death Effects")]
+    public GameObject deathEffect;
+    private float deathEffectDelay = 1f;
+
+    [Header("Death Signals")]
+    public Signal roomSignal;
+
+    private void Awake()
+    {
+        health = maxHealth.initialValue;
+    }
+
+    private void OnEnable()
+    {
+        transform.position = homePosition;
+        health = maxHealth.initialValue;
+        currentState = EnemyState.idle;
+    }
+
+    private void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            DeathEffect();
+            if (roomSignal != null)
+            {
+                roomSignal.Raise();
+            }
+            this.gameObject.SetActive(false);
+        }
+    }
+
+
+    private void DeathEffect()
+    {
+        if (deathEffect != null)
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, deathEffectDelay);
+        }
+    }
 
     public void Knock(Rigidbody2D myRigidbody, float knockTime)
     {
--- WSA1/Assets/Scripts/log.cs
+++ WSA1/Assets/Scripts/log.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class log : Enemy
 {
 
-    private Rigidbody2D myRigidbody;
+
+    public Rigidbody2D myRigidbody;
+    [Header("Target Variables")]
     public Transform target;
     public float chaseRadius;
     public float attackRadius;
-    public Transform homePosition;
+
+    [Header("Animator")]
     public Animator anim;
 
 
@@ -28,7 +31,7 @@ public class log : Enemy
         CheckDistance();
     }
 
-    void CheckDistance()
+    public virtual void CheckDistance()
     {
         if (Vector3.Distance(target.position,
                             transform.position) <= chaseRadius
@@ -39,15 +42,54 @@ public class log : Enemy
                 && currentState != EnemyState.stagger)
             {
                 Vector3 temp = Vector3.MoveTowards(transform.position,
-                                                       target.position,
+                                                         target.position,
                                                          moveSpeed * Time.deltaTime);
+                changeAnim(temp - transform.position);
                 myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
+                anim.SetBool("isWoken", true);
+            }
+        }
+        else if (Vector3.Distance(target.position,
+                           transform.position) > chaseRadius)
+        {
+            anim.SetBool("isWoken", false);
+        }
+    }
+
+    public void SetAnimFloat(Vector2 setVector)
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+
+    public void changeAnim(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x > 0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if (direction.x < 0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y))
+        {
+            if (direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if (direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
             }
         }
     }
 
-    private void ChangeState(EnemyState newState)
+    public void ChangeState(EnemyState newState)
     {
         if (currentState != newState)
         {
