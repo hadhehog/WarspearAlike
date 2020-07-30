ee1a55dbc606ca15ac3883ad93f5bcdbc71460e0
hadhehog <hadhehog@gmail.com>
2020-07-30 17:59:19 +0200

    Turret
    Added Turret

---
+++ WSA1/Assets/Scripts/Enemy/TurretProjectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretProjectile : MonoBehaviour
+{
+    public int damage = 40;
+    // int for damage
+    public float health;
+    public float speed = 20f;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+     {
+         enemyHealth enemy = hitInfo.GetComponent<enemyHealth>();
+         // Grabs the enemyHealth script component
+         if (enemy != null)
+         {
+             enemy.TakeDamage(damage);
+             Destroy(gameObject);
+         }
+
+     }
+
+}
